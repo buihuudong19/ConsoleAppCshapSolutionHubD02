8e6e7dff837d16de1f8ae81afa50d81d0bde3ba8
Dongbh <buihuudong19@gmail.com>
2023-05-08 16:39:07 +0700

    abc

---
+++ ConsoleAppOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Id = 1;
+            s1.FullName = "Dong";
+            s1.Address = "Quan 10";
+            s1.Mark = 5.5;
+
+            s1.Display();
+
+
+        }
+    }
+}
---
+++ ConsoleAppOOP/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleAppOOP
+{
+    public class Student
+    {
+        //1. fields
+        private int id;
+        private string fullName;
+
+        //2. thuoc tinh
+        public int Id { get => id; set => id = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+
+        public string Address;
+        public double Mark;
+
+        //ham
+        public void Display()
+        {
+            Console.WriteLine($"Id = {Id}, Fullname is: {FullName}, " +
+                $"Address: {Address} and Mark: {Mark}");  
+        }
+    }
+}
