060643f71b8d00dcbce13188d4c97d10c6d229b2
Dongbh <buihuudong19@gmail.com>
2023-05-22 13:43:21 +0700

    commit

---
+++ ConsoleAppOopInheritances/Shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOopInheritances
+{
+    public class Shape
+    {
+        //properties
+        public string Color { get; set;}
+        public DateTime DateCreate { get; set;}
+
+        public Shape()
+        {
+        }
+        public Shape(string color, DateTime dateCreate)
+        {
+            Color = color;
+            DateCreate = dateCreate;
+        }
+
+        public override string ToString()
+        {
+            return $"Color = {Color} and Date Create:{DateCreate}";
+        }
+
+    }
+}
